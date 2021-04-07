91d9c166e3287f0e2d7ab20cfe884fc51e38acd9
YuxLiu <liuyuxuan1230@outlook.com>
2021-04-07 17:55:24 +0800

    set query-null.ts hook for find categories.

---
+++ src/hooks/query-null.ts
@@ -0,0 +1,17 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (queryField: string): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    const {
+      params: { query = {} },
+    } = context;
+    if (query[queryField] === 'null') {
+      query[queryField] = null;
+    }
+    context.params.query = query;
+    return context;
+  };
+};
